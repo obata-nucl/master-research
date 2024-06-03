80642424a45c0557bf67501fa5a509750245078f
Obata Yutaka <obata0904@outlook.jp>
2024-06-03 15:57:17 +0900

    first commit

--- src/test.py
+++ src/test.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import subprocess
 
 #訓練データ、検証データ、テストデータの読み込み
+#(質量数, 陽子数, 中性子数, 陽子ボソン数, 中性子ボソン数, E(0_2^+), E(2_1^+), E(4_1^+), E(6_1^+))
 train_data = torch.tensor(np.loadtxt("/home/yutaka/research/data/train_data.csv",delimiter=','),dtype=torch.float32,requires_grad=True)
 val_data = torch.tensor(np.loadtxt("/home/yutaka/research/data/val_data.csv",delimiter=','),dtype=torch.float32,requires_grad=True)
 test_data = torch.tensor(np.loadtxt("/home/yutaka/research/data/test_data.csv",delimiter=','),dtype=torch.float32,requires_grad=True)
@@ -20,4 +21,3 @@ torch.manual_seed(seed)
 
 
 layer1 = nn.Linear(5,16)
-print(layer1)
